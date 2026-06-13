68fc81bbaeeb2c11840bcd6d479cd6bd4f56222c
mollar05 <leuns0711@gmail.com>
2026-06-13 21:16:24 +0900

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const tf = require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
